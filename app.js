8e59ef41ee49ebc94843369b7587f3699f3c59be
Ian Reimers <ian.reimers.apps@gmail.com>
2021-02-23 16:00:03 -0700

    Added mobile navigation menu

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const modal = document.querySelector(".modal");
 const previews = document.querySelectorAll(".gallery-container img");
 const largeImg = document.querySelector(".large-img");
 const caption = document.querySelector(".caption");
+const navLinks = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger-container").firstElementChild;
+const links = document.querySelectorAll(".nav-links li a");
+
+console.log(burger);
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-open");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (navLinks.classList.contains("nav-open")) {
+      navLinks.classList.remove("nav-open");
+    }
+  });
+});
 
 previews.forEach((preview) => {
   preview.addEventListener("click", (e) => {
