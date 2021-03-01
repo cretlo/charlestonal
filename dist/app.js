17559adbd25720dcc79688b74b55bd4b7246aef3
ianreimers <ian.reimers.apps@gmail.com>
2021-03-01 00:32:23 -0700

    Added fade to hero when content is loaded

--- dist/app.js
+++ dist/app.js
@@ -1,43 +1,53 @@
-const modal = document.querySelector(".modal");
-const previews = document.querySelectorAll(".gallery-container img");
-const largeImg = document.querySelector(".large-img");
-const caption = document.querySelector(".caption");
-const navLinks = document.querySelector(".nav-links");
-const burger = document.querySelector(".burger-container").firstElementChild;
-const links = document.querySelectorAll(".nav-links li a");
+const heroContainer = document.querySelector('.hero-container');
+
+const modal = document.querySelector('.modal');
+const previews = document.querySelectorAll('.gallery-container img');
+const largeImg = document.querySelector('.large-img');
+const caption = document.querySelector('.caption');
+const navLinks = document.querySelector('.nav-links');
+const burger = document.querySelector('.burger-container').firstElementChild;
+const links = document.querySelectorAll('.nav-links li a');
 
 console.log(burger);
 
-burger.addEventListener("click", () => {
-  navLinks.classList.toggle("nav-open");
+// Opens navigation on mobile
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('nav-open');
 });
 
+// Closes navigation on mobile
 links.forEach((link) => {
-  link.addEventListener("click", () => {
-    if (navLinks.classList.contains("nav-open")) {
-      navLinks.classList.remove("nav-open");
+  link.addEventListener('click', () => {
+    if (navLinks.classList.contains('nav-open')) {
+      navLinks.classList.remove('nav-open');
     }
   });
 });
 
+// Large image modal display
 previews.forEach((preview) => {
-  preview.addEventListener("click", (e) => {
-    const largeSrc = e.target.getAttribute("data-original");
-    const altAtr = e.target.getAttribute("alt");
+  preview.addEventListener('click', (e) => {
+    const largeSrc = e.target.getAttribute('data-original');
+    const altAtr = e.target.getAttribute('alt');
 
     largeImg.src = `${largeSrc}`;
     caption.innerHTML = altAtr;
 
-    modal.classList.add("open");
+    modal.classList.add('open');
   });
 });
 
-modal.addEventListener("click", (e) => {
+// Modal closing
+modal.addEventListener('click', (e) => {
   if (
-    e.target.classList[1] === "open" ||
-    e.target.classList[0] === "large-img"
+    e.target.classList[1] === 'open' ||
+    e.target.classList[0] === 'large-img'
   ) {
-    modal.classList.remove("open");
-    largeImg.src = "";
+    modal.classList.remove('open');
+    largeImg.src = '';
   }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  heroContainer.classList.add('active');
 });
