aa080d6c253d56738f7a57f5b68fd9de50f948e9
ianreimers <ian.reimers.apps@gmail.com>
2021-03-02 20:15:52 -0700

    went back to main js file before compiling

--- dist/app.js
+++ dist/app.js
@@ -1,52 +1,61 @@
-"use strict";
+const navLinks = document.querySelector('.nav-links');
+const burgerContainer = document.querySelector('.burger-container');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const heroContainer = document.querySelector('.hero-container');
+const modal = document.querySelector('.modal');
+const previews = document.querySelectorAll('.gallery-container img');
+const largeImg = document.querySelector('.large-img');
+const caption = document.querySelector('.caption');
+const anchorLinks = document.querySelectorAll('.nav-links li a');
 
-var navLinks = document.querySelector('.nav-links');
-var burgerContainer = document.querySelector('.burger-container');
-var open = document.getElementById('open');
-var close = document.getElementById('close');
-var heroContainer = document.querySelector('.hero-container');
-var modal = document.querySelector('.modal');
-var previews = document.querySelectorAll('.gallery-container img');
-var largeImg = document.querySelector('.large-img');
-var caption = document.querySelector('.caption');
-var anchorLinks = document.querySelectorAll('.nav-links li a'); // Opens navigation on mobile
-
-open.addEventListener('click', function () {
+// Opens navigation on mobile
+open.addEventListener('click', () => {
   navLinks.classList.toggle('nav-open');
   burgerContainer.classList.toggle('open');
-}); // Closes navigation on mobile
+});
 
-close.addEventListener('click', function () {
+// Closes navigation on mobile
+close.addEventListener('click', () => {
   navLinks.classList.toggle('nav-open');
   burgerContainer.classList.toggle('open');
-}); // Closes navigation if a link is clicked, on mobile
+});
 
-anchorLinks.forEach(function (link) {
-  link.addEventListener('click', function () {
+// Closes navigation if a link is clicked, on mobile
+anchorLinks.forEach((link) => {
+  link.addEventListener('click', () => {
     if (navLinks.classList.contains('nav-open')) {
       navLinks.classList.remove('nav-open');
       burgerContainer.classList.toggle('open');
     }
   });
-}); // Large image modal displayed when a preview is clicked
+});
 
-previews.forEach(function (preview) {
-  preview.addEventListener('click', function (e) {
-    var largeSrc = e.target.getAttribute('data-original');
-    var altAtr = e.target.getAttribute('alt');
-    largeImg.src = "".concat(largeSrc);
+// Large image modal displayed when a preview is clicked
+previews.forEach((preview) => {
+  preview.addEventListener('click', (e) => {
+    const largeSrc = e.target.getAttribute('data-original');
+    const altAtr = e.target.getAttribute('alt');
+
+    largeImg.src = `${largeSrc}`;
     caption.innerHTML = altAtr;
+
     modal.classList.add('open');
   });
-}); // Closes modal clicked anywhere
+});
 
-modal.addEventListener('click', function (e) {
-  if (e.target.classList[1] === 'open' || e.target.classList[0] === 'large-img') {
+// Closes modal clicked anywhere
+modal.addEventListener('click', (e) => {
+  if (
+    e.target.classList[1] === 'open' ||
+    e.target.classList[0] === 'large-img'
+  ) {
     modal.classList.remove('open');
     largeImg.src = '';
   }
-}); // Fades hero into view when content is loaded
+});
 
-window.addEventListener('DOMContentLoaded', function () {
+// Fades hero into view when content is loaded
+window.addEventListener('DOMContentLoaded', () => {
   heroContainer.classList.add('active');
 });
