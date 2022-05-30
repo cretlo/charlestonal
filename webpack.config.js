d359df7f0db8349aa51135cf49882b917c25e878
Ian Reimers <ian.reimers.apps@gmail.com>
2022-05-29 20:44:28 -0600

    Redesigned the site using MaterializeCss, added custom CSS, properly confirgured webpack with postcss and babel, added an attach resume input field, and switched icons with FontAwesome

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,107 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
-// Doesnt work for whatever reason
+// Mode variable depending on building or running devServer
 const webpackMode =
-  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+  process.env.Node_ENV !== undefined &&
+  process.env.NODE_ENV.trim() === 'production'
+    ? 'production'
+    : 'development';
 
 module.exports = {
   mode: 'production',
   devServer: {
-    contentBase: './dist',
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    hot: false,
+    compress: true,
+    open: true,
   },
-  target: 'web',
-  entry: './src/app.js',
+  entry: path.resolve(__dirname, 'src/index.js'),
   module: {
     rules: [
       {
-        test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env', {}]],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
       {
         test: /\.html$/i,
         loader: 'html-loader',
       },
       {
-        test: /\.(jpg|png|jpeg|svg)$/,
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: 'ie 11', useBuiltIns: 'usage', corejs: '3.22.7' },
+              ],
+            ],
+          },
         },
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+          options: {
+            encodeOptions: {
+              mozjpeg: {
+                quality: 50,
+              },
+            },
+          },
+        },
+      }),
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/template.html',
+      template: './src/index.html',
+      filename: 'index.html',
     }),
+    // new BundleAnalyzerPlugin(),
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[contenthash].bundle.js',
-    assetModuleFilename: 'images/[name].[hash][ext][query]',
+    filename: '[name][contenthash].js',
+    assetModuleFilename: 'images/[name][hash][ext][query]',
+    environment: {
+      arrowFunction: false,
+    },
+    publicPath: '/dist/',
   },
 };
